4243677a97f695c833a8cbc14e54fa4b7980f07b
kuylim <kuylim.tith@gmail.com>
2022-11-21 12:21:08 -0600

    #Added - Share module

---
+++ share-module/src/test/java/edu/miu/sharemodule/ShareModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.sharemodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
