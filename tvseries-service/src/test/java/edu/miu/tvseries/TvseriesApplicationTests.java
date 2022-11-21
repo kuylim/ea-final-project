73a79577aebb18eed216150bdaebef858ff1157b
kuylim <kuylim.tith@gmail.com>
2022-11-21 12:12:36 -0600

    #Initial Project

---
+++ tvseries-service/src/test/java/edu/miu/tvseries/TvseriesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.tvseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvseriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
